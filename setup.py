a13d2b24ae6710213dead83ae14b11c2f06b7a86
Bhargav Srinivasa <bhargavvader@gmail.com>
2017-07-27 09:52:34 +0200

    Update to v0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup
 
-version = "0.1.0"
+version = "0.2.0"
 setup(name='pycobra',
       version=version,
       description='Python implementation of COBRA algorithm with regression analysis',
